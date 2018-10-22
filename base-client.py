aaaf201e76d3a6e8afa5d6280f646ad8fcb1f66f
சகாயன் சிற்சபேசன் <ssit662@aucklanduni.ac.nz>
2018-10-22 21:34:18 +1300

    Add logging functionality to base client

--- base-client.py
+++ base-client.py
@@ -8,6 +8,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from time import sleep
+import os.path
+import os
+import datetime
+import time
+
+def write_to_file(x, y, o):
+    if not os.path.isfile('log.csv'):
+        with open('log.csv','w+') as fd:
+            fd.write("Time:, X:, Y:, Orientation:" + os.linesep)
+    with open('log.csv','a') as fd:
+        timestep = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        fd.write(str(timestep) + ', ' + str(x) + ', ' + str(y) + ', ' + str(o) + os.linesep)
 
 # Serial ports
 locations=['/dev/ttyACM0', '/dev/ttyACM1','/dev/ttyACM2', '/dev/ttyACM3','/dev/ttyACM4', '/dev/ttyACM5','/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3', '/dev/ttyUSB4', '/dev/ttyUSB5', '/dev/ttyUSB6', '/dev/ttyUSB7', '/dev/ttyUSB8', '/dev/ttyUSB9', '/dev/ttyUSB10','/dev/ttyS0', '/dev/ttyS1', '/dev/ttyS2', 'com2', 'com3', 'com4', 'com5', 'com6', 'com7', 'com8', 'com9', 'com10', 'com11', 'com12', 'com13', 'com14', 'com15', 'com16', 'com17', 'com18', 'com19', 'com20', 'com21', 'com1', 'end']
@@ -46,8 +58,12 @@ def update_line(num, dsa, line):
         coords=[[0], [0]]
         coords[0][0] = int(coord[0])
         coords[1][0] = int(coord[1])
-        new = np.asarray(coords)        
-        print("Current Location: X: " + str(new[0][0]) + ", Y: " + str(new[1][0]) + ", Orientation: " + new_data[1].strip())
+        new = np.asarray(coords)
+        x_loc = str(new[0][0])
+        y_loc = str(new[1][0])
+        ore = new_data[1].strip()
+        print("Current Location: X: " + x_loc + ", Y: " + y_loc + ", Orientation: " + ore)
+        write_to_file(x_loc, y_loc, ore)
         data = np.append(data, new, 1)
     except:
         pass
